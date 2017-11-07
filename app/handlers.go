1735c05a1a7317a43d3f7856bfb6398a597bd36a
Aidan Carson <aidancarson@10-249-18-245.wireless.oregonstate.edu>
2017-11-07 15:11:19 -0800

    Sanitize Code

--- app/handlers.go
+++ app/handlers.go
@@ -12,17 +12,19 @@ import (
 )
 
 //
-//  handlers.go
-//  ContentService
+// handlers.go
+// ContentService
 //
-//  Copyright © 2017 NGINX Inc. All rights reserved.
+// Copyright © 2017 NGINX Inc. All rights reserved.
 //
 
+// Struct object for storing photo information within RethinkDB
 type Photo struct {
 	Name	string	`gorethink:"name"`
 	Url		string	`gorethink:"url"`
 }
 
+// Struct object for storing post information within RethinkDB
 type Post struct {
 	Id		  string	`gorethink:"id,omitempty"`
 	Date      time.Time	`gorethink:"date" json:"date"`
@@ -34,18 +36,25 @@ type Post struct {
 	Body      string	`gorethink:"body,omitempty" json:"body"`
 }
 
+// Environment object used to inject database state into handlers
+// Practically used to test handlers with a mock database
+// (Should remove IsTest in future releases when mux is fixed)
 type Env struct {
 	Session  db.QueryExecutor
 	IsTest	 bool
 }
 
+// Handler object used for allowing handler functions to accept
+// an environment object
 type Handler struct {
 	*Env
 	H func(e *Env, w http.ResponseWriter, r *http.Request) error
 }
-
+// HandlerFunc type used to specify a template for handler functions to follow
 type HandlerFunc func(e *Env, w http.ResponseWriter, r *http.Request) error
 
+// ServeHTTP is called on each HTTP request. Species which function is
+// called as well as how errors are handled and how logging is set
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := h.H(h.Env, w, r)
 	if err != nil {
@@ -64,15 +73,23 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Handler listening for GET at "/" URI
+// Returns specified string
+// @return: string "Welcome to the content service!"
 func Welcome(env *Env, w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprint(w, "Welcome to the content service!")
 	return nil
 }
 
-func Articles(env *Env, w http.ResponseWriter, r *http.Request) error {
+// Handler listening for GET at "/v1/content" URI
+// Get array of all articles in database
+// @return: array of JSON objects, each element with single post information
+func GetAllArticles(env *Env, w http.ResponseWriter, r *http.Request) error {
 	var resp *db.Cursor
 	var err error
 
+	// Call database and get all articles with fields:
+	// id, date, location, author, photo, title, and extract
 	resp, err = db.DB("content").Table("posts").WithFields("id", "date", "location", "author", "photo", "title", "extract").Run(env.Session)
 	if err != nil {
 		fmt.Print(err)
@@ -81,12 +98,14 @@ func Articles(env *Env, w http.ResponseWriter, r *http.Request) error {
 	defer resp.Close()
 
 	var posts []interface{}
+	// Map all posts into an array of interfaces for printing to user
 	err = resp.All(&posts)
 	if err != nil {
 		fmt.Printf("Error scanning database result: %s", err)
 		return StatusError{500, err}
 	}
 
+	// Write header to specify returning a JSON object
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(posts); err != nil {
@@ -97,14 +116,20 @@ func Articles(env *Env, w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func Article(env *Env, w http.ResponseWriter, r *http.Request) error {
-	vars := mux.Vars(r)
-	var articleId string
+// Handler listening for GET at "/v1/content/{articleID}" URI
+// Get specified article
+// Parameters: articleID - specifies which article to get
+// @return: single JSON object with post information
+func GetArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
 	var resp *db.Cursor
 	var err error
 
-	articleId = vars["articleId"]
+	// Read gorilla/mux variables for article ID to fetch from database
+	vars := mux.Vars(r)
+	var articleId string = vars["articleId"]
 
+	// IsTest variable shouldn't be necessary, but setting mux variables
+	// must be set correctly in tests in order to avoid this
 	if env.IsTest {
 		resp, err = db.DB("content").Table("posts").Get(strings.Split(r.URL.Path, "/")[3]).Pluck("id", "date", "location", "author", "photo", "title", "body").Run(env.Session)
 	} else {
@@ -118,12 +143,14 @@ func Article(env *Env, w http.ResponseWriter, r *http.Request) error {
 	defer resp.Close()
 
 	var post []interface{}
+	// Map all posts into an array of interfaces for printing to user
 	err = resp.All(&post)
 	if err != nil {
 		fmt.Printf("Error scanning database result: %s", err)
 		return StatusError{500, err}
 	}
 
+	// Write header to specify returning a JSON object
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(post); err != nil {
@@ -132,16 +159,24 @@ func Article(env *Env, w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// Handler listening for POST at "/v1/content" URI
+// Creates new article based on JSON object in POST
+// @POST: new post object
+// @return: ID of post within database
 func NewArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
-	decoder := json.NewDecoder(r.Body)
-	var newPost Post
 	var resp db.WriteResponse
+
+	// Decode request body into Post object
+	var newPost Post
+	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newPost)
 	if err != nil {
 		panic(err)
 	}
 	defer r.Body.Close()
 
+	// IsTest variable shouldn't be necessary, but setting mux variables
+	// must be set correctly in tests in order to avoid this
 	if env.IsTest {
 		resp, err = db.DB("content").Table("posts").Insert(newPost).RunWrite(env.Session)
 	} else {
@@ -153,25 +188,37 @@ func NewArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
 		return StatusError{500, err}
 	}
 
+	// Return number of rows inserted into database and keys generated
 	fmt.Printf("%d row inserted, %d key generated", resp.Inserted, len(resp.GeneratedKeys))
 	fmt.Fprint(w, resp.GeneratedKeys)
 
 	return nil
 }
 
+// Handler listening for PUT at "/v1/content/{articleID}" URI
+// Updates elements within specified post
+// Parameters: articleID - specifies which article to update
+// @POST: new post object
+// @return: JSON object that specied what information was changed
 func ReplaceArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
-	vars := mux.Vars(r)
-	var articleId string
-	articleId = vars["articleId"]
 	var resp db.WriteResponse
 
-	decoder := json.NewDecoder(r.Body)
+	// Read gorilla/mux variables for article ID to fetch from database
+	vars := mux.Vars(r)
+	var articleId string = vars["articleId"]
+
+	// Decode request body into Post object
 	var newPost Post
+	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newPost)
 	if err != nil {
 		panic(err)
 	}
+
 	defer r.Body.Close()
+
+	// IsTest variable shouldn't be necessary, but setting mux variables
+	// must be set correctly in tests in order to avoid this
 	if env.IsTest {
 		resp, err = db.DB("content").Table("posts").Get(strings.Split(r.URL.Path, "/")[3]).Replace(newPost).RunWrite(env.Session)
 	} else {
@@ -184,26 +231,37 @@ func ReplaceArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
 		return StatusError{500, err}
 	}
 
+	// Print JSON response
 	printObj(w, resp)
 
 	return nil
 }
 
+// Handler listening for PATCH at "/v1/content/{articleId}/{element}/{newValue}" URI
+// Updates single element within specified post
+// Parameters: articleID - specifies which article to update
+// 			   element - element within post to update
+// 			   newValue - new value of element
+// @return: JSON object that specied what information was changed
 func UpdateArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
+	var resp db.WriteResponse
+	var err error
+
+	// Read gorilla/mux variables for article ID and element + value to update database
 	vars := mux.Vars(r)
 	var articleId string = vars["articleId"]
 	var element string = vars["element"]
 	var newValue string = vars["newValue"]
 
-	var resp db.WriteResponse
-	var err error
-
+	// Set syntax for new/updated element within database
 	str := `{"` + element +`": "` + newValue + `"}`
-	fmt.Print(str)
+
+	// Unmarshal str variable into Post object
 	res := Post{}
 	json.Unmarshal([]byte(str), &res)
-	fmt.Println(res)
 
+	// IsTest variable shouldn't be necessary, but setting mux variables
+	// must be set correctly in tests in order to avoid this
 	if env.IsTest {
 		s := strings.Split(r.URL.Path, "/")
 		resp, err = db.DB("content").Table("posts").Get(s[3]).Update(`{"` + s[4] +`": "` + s[5] + `"}`).RunWrite(env.Session)
@@ -215,18 +273,26 @@ func UpdateArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
 		return StatusError{500, err}
 	}
 
+	// Print JSON response
 	printObj(w, resp)
 
 	return nil
 }
 
+// Handler listening for DELETE at "/v1/content/{articleId}" URI
+// Delete specified post
+// Parameters: articleID - specifies which article to delete
+// @return: empty JSON object
 func DeleteArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
-	vars := mux.Vars(r)
-	var articleId string = vars["articleId"]
-
 	var resp *db.Cursor
 	var err error
 
+	// Read gorilla/mux variables for article ID and element + value to update database
+	vars := mux.Vars(r)
+	var articleId string = vars["articleId"]
+
+	// IsTest variable shouldn't be necessary, but setting mux variables
+	// must be set correctly in tests in order to avoid this
 	if env.IsTest {
 		resp, err = db.DB("content").Table("posts").Get(strings.Split(r.URL.Path, "/")[3]).Delete().Run(env.Session)
 	} else {
@@ -239,11 +305,13 @@ func DeleteArticle(env *Env, w http.ResponseWriter, r *http.Request) error {
 
 	defer resp.Close()
 
+	// Print JSON response
 	printObj(w, resp)
 
 	return nil
 }
 
+// Print JSON object to screen
 func printObj(w http.ResponseWriter, v interface{}) {
 	vBytes, _ := json.Marshal(v)
 	fmt.Fprint(w, string(vBytes))
--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	db "gopkg.in/gorethink/gorethink.v3"
-	"github.com/joho/godotenv"
 	"net/http"
 	"log"
 	"os"
@@ -10,33 +9,35 @@ import (
 )
 
 //
-//  main.go
-//  ContentService
+// main.go
+// ContentService
 //
-//  Copyright © 2017 NGINX Inc. All rights reserved.
+// Copyright © 2017 NGINX Inc. All rights reserved.
 //
 
+// Main function responsible for calling initializer functions for
+// establishing HTTP endpoints with app listening on port :8080
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	var session *db.Session
 
 	fmt.Print(os.Getenv("RETHINKDB_URL"))
-	session, err = db.Connect(db.ConnectOpts{
+
+	// Initialize session variable by connecting to RethinkDB database
+	// Specified by "RETHINKDB_URL" environment variable
+	session, err := db.Connect(db.ConnectOpts{
 		Address: os.Getenv("RETHINKDB_URL"),
 	})
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
 
+	// Initialize environment variable to inject into handlers.
+	// IsTest set to false because this is a production environment (will remove in later release)
 	env := &Env{
 		Session: session,
 		IsTest: false,
     }
 
+	// Create database called "content" for storing articles
 	resp, err := db.DBCreate("content").RunWrite(env.Session)
 	if err != nil {
 		fmt.Print(err)
@@ -44,6 +45,7 @@ func main() {
 
 	fmt.Printf("%d DB created", resp.DBsCreated)
 
+	// Create table called "posts" within database
 	response, err := db.DB("content").TableCreate("posts").RunWrite(env.Session)
 	if err != nil {
 		log.Print("Error creating table: " + err.Error())
@@ -51,7 +53,10 @@ func main() {
 
 	fmt.Printf("%d table created", response.TablesCreated)
 
+	// Initialize router for mapping functions within handlers.go
+	// to HTTP endpoints at specified URIs within routes.go
 	router := NewRouter(env)
 
+	// Listen for requests on port :8080 with router and logging
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
