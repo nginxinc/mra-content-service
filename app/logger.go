78e992f0267f369d644ad59f6eee3c7f6202dafe
cpretzer <charles@nginx.com>
2018-01-01 16:21:29 -0800

    * Merge from develop branch

--- app/logger.go
+++ app/logger.go
@@ -7,18 +7,20 @@ import (
 )
 
 //
-//  logger.go
-//  ContentService
+// logger.go
+// ContentService
 //
-//  Copyright © 2017 NGINX Inc. All rights reserved.
+// Copyright © 2017 NGINX Inc. All rights reserved.
 //
 
+// Function for logging incoming request information
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		inner.ServeHTTP(w, r)
 
+		// Log request method, URL, name of function, and current time
 		log.Printf(
 			"%s\t%s\t%s\t%s",
 			r.Method,
