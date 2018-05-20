ca2aac41b6f73aed6c4c203e2a811dddd86251c5
Eduard Borges <eduardborges@me.com>
2018-05-20 08:22:08 +0200

    MRADEV-911_MRADEV-912_MRADEV-913 router mesh support

--- app/start.sh
+++ app/start.sh
@@ -7,26 +7,36 @@ if [ ! -f .env ]; then
     touch .env
 fi
 
-case "$NETWORK" in
-    fabric)
-        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
-        echo 'Fabric configuration set'
-        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
-        ;;
-    router-mesh)
-        ;;
-    *)
-        echo 'Network not supported'
-esac
-
-su content-service -c "$APP"
+su content-service -c "$APP" &
 
 sleep 10
 #APP gets rendered as go
 APP_PID=`ps aux | grep "$APP" | grep -v grep`
 
-while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
-do
-	sleep 5;
-	APP_PID=`ps aux | grep "$APP" | grep -v grep`;
-done
+case "$NETWORK" in
+    fabric)
+        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
+        echo 'Fabric configuration set'
+        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
+
+        sleep 30
+
+        while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
+        ;;
+    router-mesh)
+        sleep 30
+
+        while [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
+        ;;
+    *)
+        echo 'Network not supported'
+        exit 1
+esac
